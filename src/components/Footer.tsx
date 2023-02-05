a6dfd98f81635341085032dedd696fa8a2e4017a
Zack Sheppard <zack@zacksheppard.com>
2023-02-05 14:48:53 +0000

    Migrate globe to use SVGR

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
 import Link from "next/link";
+import Globe from "../../public/img/globe.svg";
 
 export default function Footer() {
   return (
     <footer className="text-gray-600 body-font">
       <div className="bg-gray-100">
         <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 h-10 relative w-10">
-            <Image src="/img/globe.svg" alt="A happy little globe" />
-          </a>
+          <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 h-10 relative w-10">
+            <Globe width={40} className="text-black" />
+          </div>
           <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
             © 2021 Traveler Dev Ltd (Registered England 13120175)
           </p>
--- src/components/ImagePane.tsx
+++ src/components/ImagePane.tsx
@@ -1,16 +1,11 @@
-import Image from "next/image";
 import React from "react";
+import Globe from "../../public/img/globe.svg";
 
 const ImagePane = () => {
   return (
     <div className="lg:w-1/2 w-full my-10 lg:my-0 flex flex-col-reverse lg:flex-col items-center justify-center">
-      <div className="w-full md:max-w-sm relative text-current h-56">
-        <div className="block dark:hidden">
-          <Image alt="Traveler's Happy Globe" src="/img/globe-black.svg" />
-        </div>
-        <div className="hidden dark:block">
-          <Image alt="Traveler's Happy Globe" src="/img/globe.svg" />
-        </div>
+      <div className="w-full md:max-w-sm relative text-current h-56 flex justify-center items-center text-black dark:text-white">
+        <Globe className="w-56 h-56" aria-label="A happy little globe" />
       </div>
       <h1 className="font-title font-bold text-5xl xl:text-6x">Traveler Dev</h1>
       <p className="font-title mt-3 text-xl text-gray-500">Quality software will travel</p>
